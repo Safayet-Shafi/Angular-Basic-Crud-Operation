407563d4539daf7b516c39f19d397f9492d1e3f0
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2023-12-23 17:52:52 +0600

    update

--- src/app/employee-dash-board/employee-dash-board.component.ts
+++ src/app/employee-dash-board/employee-dash-board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeModel } from './model/employee-dashboard-model';
+import { EmployeeDashboardApiService } from '../api/employee-dashboard-api.service';
 
 @Component({
   selector: 'app-employee-dash-board',
@@ -7,12 +9,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./employee-dash-board.component.css']
 })
 export class EmployeeDashBoardComponent implements OnInit {
-  
+  employeeModelObj : EmployeeModel=new EmployeeModel();
   formValue!:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  employeeData:any;
+  constructor(private formBuilder:FormBuilder,
+              private employeeApiService:EmployeeDashboardApiService) { }
 
   ngOnInit(): void {
-
+    
+    this.getEmployee();
     this.formValue=this.formBuilder.group({
       firstName:[''],
       lastName:[''],
@@ -21,6 +26,35 @@ export class EmployeeDashBoardComponent implements OnInit {
       salary:[''],
     })
   }
-  
+
+  postEmployee(){
+    this.employeeModelObj.firstName=this.formValue.value.firstName;
+    this.employeeModelObj.lasttName=this.formValue.value.lastName;
+    this.employeeModelObj.mobile=this.formValue.value.mobile;
+    this.employeeModelObj.email=this.formValue.value.email;
+    this.employeeModelObj.salary=this.formValue.value.salary;
+
+    this.employeeApiService.postEmployee(this.employeeModelObj)
+    .subscribe(res=>{
+      console.log(res);
+      alert("Data Added")
+      this.formValue.reset();
+      let ref=document.getElementById("close")
+      ref?.click();
+      this.getEmployee();
+    },
+    err=>{
+      alert("Failed")
+    }
+    )
+  }
+
+
+  getEmployee(){
+    this.employeeApiService.getEmployee()
+    .subscribe(res=>{
+      this.employeeData=res;
+    })
+  }
 
 }
---
+++ src/app/employee-dash-board/model/employee-dashboard-model.ts
@@ -0,0 +1,8 @@
+export class EmployeeModel{
+     id:number=0;
+     firstName:string='';
+     lasttName:string='';
+     mobile:string='';
+     email:string='';
+     salary:string='';
+}
