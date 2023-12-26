aabb1d7b7a38406f331d339ff879564371b09983
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2023-12-27 00:30:03 +0600

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { EmployeeDashBoardComponent } from './employee-dash-board/employee-dash-board.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeDashBoardComponent
+    EmployeeDashBoardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
