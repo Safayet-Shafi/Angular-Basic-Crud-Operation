e05e320cbb15ffb0cd0dcac9984976c6bbec5fc4
Safayet-Shafi <77004144+Safayet-Shafi@users.noreply.github.com>
2023-12-30 02:58:07 +0600

    login sign up implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    loginValue!:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,
+    private http: HttpClient,
+    private router :Router,) { }
 
   ngOnInit(): void {
+    this.loginValue=this.formBuilder.group({
+      email:[''],
+      password:[''],
+    })
+  }
+
+  login(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      console.log(res);
+      const user=res.find((a:any)=>{
+        return a.email === this.loginValue.value.email &&  a.password === this.loginValue.value.password
+      });
+      if(user){
+        alert("Login Successful");
+        this.loginValue.reset();
+        this.router.navigate(['dashboard'])
+      }
+      else{
+        alert("User Not Found");
+      }
+    },err=>{
+      alert("Server Error");
+    })
+  
+
   }
 
 }
